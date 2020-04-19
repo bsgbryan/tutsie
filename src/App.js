95447023fd609795e9c3087e1ed59fd6884843ae
bsgbryan <bsgbryan@gmail.com>
2020-04-19 16:03:59 -0500

    Added Slate dependency, started working through tutorials

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import React dependencies.
+import React, { useEffect, useMemo, useState } from "react"
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+
+// Define our app...
+const App = () => {
+  // Create a Slate editor object that won't change across renders.
+  const editor = useMemo(() => withReact(createEditor()), [])
+
+  // Keep track of state for the value of the editor.
+  const [value, setValue] = useState([{
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  }])
+
+  return <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+    <Editable />
+  </Slate>
 }
 
-export default App;
+export default App
